85c277e3c909888d39ef6c3adcd88147139d0fa4
Gabriel Rodolfo Rabello <gabrielrodolforabello@gmail.com>
2024-06-20 13:14:27 -0300

    Add files via upload

---
+++ src/components/PlaylistModal.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { View, StyleSheet, Modal, Dimensions, TextInput, TouchableWithoutFeedback, Text } from "react-native";
+import { Entypo } from '@expo/vector-icons';
+
+export default function PlaylistModal({ visible, onClose }) {
+    if (!visible) return null; // Renderiza null se o modal não estiver visível
+
+    return (
+        <Modal visible={visible} animationType="slide" transparent>
+            <TouchableWithoutFeedback onPress={onClose}>
+                <View style={Styles.modalBg}></View>
+            </TouchableWithoutFeedback>
+            <View style={Styles.modalContainer}>
+                <View style={Styles.modalContent}>
+                    <Text style={Styles.text}>Criar Nova Playlist</Text>
+                    <View style={Styles.inputContainer}>
+                        <TextInput style={Styles.input} />
+                        <View style={Styles.submitContainer}>
+                            <Entypo name="check" size={30} color="green" />
+                        </View>
+                    </View>
+                </View>
+            </View>
+        </Modal>
+    )
+}
+
+const { width, height } = Dimensions.get("window");
+
+const Styles = StyleSheet.create({
+    modalContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    modalContent: {
+        backgroundColor: "#ffffff",
+        width: width - 40,
+        borderRadius: 20,
+        paddingHorizontal: 20,
+        paddingBottom: 20,
+    },
+    inputContainer: {
+        borderRadius: 20,
+        backgroundColor: "#fff",
+        justifyContent: "center",
+        alignItems: "center",
+        paddingVertical: 20,
+    },
+    input: {
+        width: "100%",
+        borderBottomWidth: 2,
+        borderBottomColor: "#febc2a",
+        fontSize: 18,
+        paddingVertical: 8,
+    },
+    submitContainer: {
+        marginTop: 15,
+        padding: 10,
+        backgroundColor: "#fffff",
+        borderRadius: 50,
+    },
+    modalBg: {
+        position: "absolute",
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
+    },
+    text: {
+        color: "orange",
+        fontWeight: "bold",
+        fontSize: 20,
+        textAlign: "center",
+        marginTop: 20,
+    },
+});
---
+++ src/pages/Playlist.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { Text, StyleSheet, ScrollView } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import PlaylistModal from "../components/PlaylistModal";
+
+export default function Playlist() {
+    const [modalVisible, setModalVisible] = useState(false)
+
+    return (
+        <ScrollView style={styles.container}>
+            <TouchableOpacity style={styles.PlayListBanner}>
+                <Text>Favoritos</Text>
+                <Text style={styles.audioCount}>0 Músicas</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => setModalVisible(true)}>
+                <Text style={styles.PlayListBtn}>+ Adicionar nova Playlist</Text>
+            </TouchableOpacity>
+            <PlaylistModal visible={modalVisible}
+            onClose={() => setModalVisible(false)}/>
+        </ScrollView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 20,
+    },
+    PlayListBanner: {
+        padding: 5,
+        backgroundColor: "#D0D1D3",
+        borderRadius: 5,
+    },
+    audioCount: {
+        marginTop: 2,
+        opacity: 0.5,
+        fontSize: 14,
+    },
+
+    PlayListBtn: {
+        color: "#febc2a",
+        letterSpacing: 1,
+        fontWeight: "bold",
+        fontSize: 14,
+    },
+});
